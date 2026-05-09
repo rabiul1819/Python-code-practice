fe37e49515b4b96c75cb65fdc50c3bd52d2b6dde
S M Rabiul <rabiulislam1819@gmail.com>
2026-05-09 20:20:45 +0600

    Class Method Properties

---
+++ Anisul Islam/oop/class_method/Create_A_Method_With_Parameters.py
@@ -0,0 +1,10 @@
+class Calculator:
+    def add(self,a,b):
+        return a+b
+
+    def Multiply(self,a,b):
+        return a*b
+
+calc = Calculator()
+print(calc.add(1,2))
+print(calc.Multiply(3,4))
